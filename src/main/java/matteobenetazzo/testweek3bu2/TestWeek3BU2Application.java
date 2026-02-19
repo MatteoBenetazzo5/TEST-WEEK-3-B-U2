8f66f578320bbcd3e8f39af5826297814a955231
MatteoBenetazzo5 <matteobenetazzo5@gmail.com>
2026-02-19 14:22:45 +0100

    Creazione progetto del venerdì

---
+++ src/main/java/matteobenetazzo/testweek3bu2/TestWeek3BU2Application.java
@@ -0,0 +1,13 @@
+package matteobenetazzo.testweek3bu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeek3BU2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWeek3BU2Application.class, args);
+    }
+
+}
---
+++ src/test/java/matteobenetazzo/testweek3bu2/TestWeek3BU2ApplicationTests.java
@@ -0,0 +1,13 @@
+package matteobenetazzo.testweek3bu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestWeek3BU2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
