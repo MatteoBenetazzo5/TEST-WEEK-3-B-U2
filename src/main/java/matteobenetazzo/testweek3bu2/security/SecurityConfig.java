7e91d88e7121c55a8d5837ad6519734afb4b88be
MatteoBenetazzo5 <matteobenetazzo5@gmail.com>
2026-02-20 17:14:15 +0100

    Ultime modifiche e screen di postman e pgAdmin

--- src/main/java/matteobenetazzo/testweek3bu2/security/SecurityConfig.java
+++ src/main/java/matteobenetazzo/testweek3bu2/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package matteobenetazzo.testweek3bu2.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -12,20 +13,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, JWTCheckerFilter jwtCheckerFilter) throws Exception {
         http
-                // disabilito CSRF perché uso API REST
                 .csrf(csrf -> csrf.disable())
-
-                // inserisco il filtro deciso da me
                 .addFilterBefore(jwtCheckerFilter, UsernamePasswordAuthenticationFilter.class)
-
                 .authorizeHttpRequests(auth -> auth
-                        // lascio liberi register e login
                         .requestMatchers("/auth/**").permitAll()
-                        // tutto il resto lo proteggo: serve autenticazione
+                        .requestMatchers(HttpMethod.POST, "/utenti").permitAll()
                         .anyRequest().authenticated()
                 );
 
-        // restituisco la configurazione finale
         return http.build();
     }
 }
